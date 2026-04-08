7503b64b306c83e1d90a965e4db03f5aa2471784
Gabriel Martins Fortunato de Paula <gabrielmartins180706@gmail.com>
2026-04-08 19:03:23 -0300

    Começando a estrutura do código

--- war.c
+++ war.c
@@ -12,3 +12,10 @@
 #define MAX_TERRITORIOS 5
 #define MAX_NOME 30
 #define MAX_MISSOES 2
+
+// Estrutura
+typedef struct {
+    char nome[MAX_NOME];
+    char cor[20];
+    int tropas;
+} Territorio;
